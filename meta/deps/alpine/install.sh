592575de5bfa6e535a6a2dd2ca6f47b521ca3503
Joe OpenSrc <joe-opensrc@protonmail.ch>
2021-07-23 11:09:36 +0200

    add coreutils to alpine dependencies...

--- meta/deps/alpine/install.sh
+++ meta/deps/alpine/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apps="gawk vim git dmenu w3m elinks wget grep less"
+apps="gawk vim git dmenu w3m elinks wget grep less coreutils"
 
 apk add --update ${apps} # that's right, no quotes! Haxx0r l337! :)
