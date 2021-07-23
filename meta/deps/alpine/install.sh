dad570770984b34ce124733a0567ded3a5294b70
Joe OpenSrc <joe-opensrc@protonmail.ch>
2021-07-23 12:35:38 +0200

    add openssl as dependency

--- meta/deps/alpine/install.sh
+++ meta/deps/alpine/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apps="gawk vim git dmenu w3m elinks wget grep less coreutils"
+apps="gawk vim git dmenu w3m elinks wget grep less coreutils openssl"
 
 apk add --update ${apps} # that's right, no quotes! Haxx0r l337! :)
