0a176acd5b45be5a80fa235ba972aa10dfa8c2be
Joe OpenSrc <joe-opensrc@protonmail.ch>
2021-07-24 11:35:48 +0200

    add tree as a dependency

--- meta/deps/alpine/install.sh
+++ meta/deps/alpine/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apps="gawk vim git dmenu w3m elinks wget grep less coreutils openssl"
+apps="gawk vim git dmenu w3m elinks wget grep less coreutils openssl tree"
 
 apk add --update ${apps} # that's right, no quotes! Haxx0r l337! :)
--- meta/deps/debian/install.sh
+++ meta/deps/debian/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apps="gawk fzf vim git links2 w3m elinks wget grep"
+apps="gawk fzf vim git links2 w3m elinks wget grep tree"
 
 apt-get update 
 apt-get install --no-install-recommends ${apps} # that's right, no quotes! Haxx0r l337! :)
